56b8d8e48379853b009bfa80d3b72082d637d762
milind <milind.mahadik@gmail.com>
2019-01-03 20:10:18 +1100

    Mode of values in R

---
+++ r-24hrs/ch03_00_understanding_data_mode.R
@@ -0,0 +1,27 @@
+# Mode of data in R
+# Data type of values
+# Numeric Values, Character Strings, Logical, Complex Numbers
+
+# How to get mode of a value in variable
+variable1 <- "Hello World!"
+print(variable1)
+mode(variable1)
+
+variable1 <- 10.1 
+print(variable1)
+mode(variable1)
+
+variable1 <- 10 
+print(variable1)
+mode(variable1)
+
+variable1 <- 3 == 4 
+print(variable1)
+mode(variable1)
+
+variable1 <- 3i + 3
+print(variable1)
+mode(variable1)
+
+
+
