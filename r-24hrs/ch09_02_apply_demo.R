f0e3b2e5ccbeee3e62b93d864d10c88f37193a76
mmilind <milind.mahadik@gmail.com>
2019-01-09 14:30:10 +1100

    Demo of apply on 3D array.

--- r-24hrs/ch09_02_apply_demo.R
+++ r-24hrs/ch09_02_apply_demo.R
@@ -14,3 +14,18 @@ apply(arr3d, c(1, 3), sum)
 apply(arr3d, c(2, 3), length)
 apply(arr3d, c(1, 3), length)
 
+
+arr3d <- array(1:60, 
+               dim=c(10, 3, 2), 
+               dimnames=list(paste("stu", 1:10, sep=""),
+                             paste("sub", 1:3, sep=""),
+                             paste("batch", 1:2, sep="")))
+arr3d[1,3,2] <- NA
+arr3d[4,2,1] <- NA
+arr3d[7,2,2] <- NA
+
+print(arr3d)
+
+apply(arr3d, c(2, 3), sum, na.rm=TRUE)
+apply(arr3d, c(1, 3), sum, na.rm=TRUE)
+
